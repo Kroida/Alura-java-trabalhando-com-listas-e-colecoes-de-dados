2eab1111ca43bb87beac4d43e4745929af69202f
Luis <luisgns8022@gmail.com>
2026-06-04 14:53:58 -0300

    Entendendo a hierarquia de classes e métodos

--- 2914-java-screenmatch-listas-colecoes/src/br/com/alura/screenmatch/modelos/Filme.java
+++ 2914-java-screenmatch-listas-colecoes/src/br/com/alura/screenmatch/modelos/Filme.java
@@ -17,4 +17,10 @@ public class Filme extends Titulo implements Classificavel {
     public int getClassificacao() {
         return (int) pegaMedia() / 2;
     }
+
+    // Método parar formatar to.String da listaDeFilmes
+    @Override
+    public String toString() {
+        return "Filme: " + this.getNome() + "(" + this.getAnoDeLancamento() + ")";
+    }
 }
