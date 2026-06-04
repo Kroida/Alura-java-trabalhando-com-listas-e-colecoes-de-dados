bc69558470251d7a5c06a46b84d04f674e11eaf8
Luis <luisgns8022@gmail.com>
2026-06-04 20:02:41 -0300

    Tipos diferentes de objetos na lista

--- 2914-java-screenmatch-listas-colecoes/src/br/com/alura/screenmatch/modelos/Filme.java
+++ 2914-java-screenmatch-listas-colecoes/src/br/com/alura/screenmatch/modelos/Filme.java
@@ -5,7 +5,7 @@ import br.com.alura.screenmatch.calculos.Classificavel;
 public class Filme extends Titulo implements Classificavel {
     private String diretor;
 
-    public Serie(String nome, int anoDeLancamento) {
+    public Filme(String nome, int anoDeLancamento) {
         super(nome, anoDeLancamento);
     }
 
