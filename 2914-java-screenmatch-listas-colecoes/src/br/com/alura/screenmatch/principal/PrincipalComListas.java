591461b717d232e971c050274d46124f8b101dbe
Luis <luisgns8022@gmail.com>
2026-06-04 20:11:40 -0300

    Identificando o objeto

--- 2914-java-screenmatch-listas-colecoes/src/br/com/alura/screenmatch/principal/PrincipalComListas.java
+++ 2914-java-screenmatch-listas-colecoes/src/br/com/alura/screenmatch/principal/PrincipalComListas.java
@@ -24,9 +24,10 @@ public class PrincipalComListas {
 
         // foreach no java
         for (Titulo item : lista) {
-            System.out.println(item.getNome());
-            Filme filme = (Filme) item;
-            System.out.println("Classificação: " + filme.getClassificacao());
+//            System.out.println(item.getNome());
+            if (item instanceof Filme filme && filme.getClassificacao() > 2) {
+                System.out.println("Classificação: " + filme.getClassificacao());
+            }
         }
     }
 }
