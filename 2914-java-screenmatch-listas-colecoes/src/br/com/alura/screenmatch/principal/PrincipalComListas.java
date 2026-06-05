cad896026fccf4c3264e6e4aee7af7e506de32dc
Luis <luisgns8022@gmail.com>
2026-06-05 00:48:26 -0300

    Utilizando a interface List

--- 2914-java-screenmatch-listas-colecoes/src/br/com/alura/screenmatch/principal/PrincipalComListas.java
+++ 2914-java-screenmatch-listas-colecoes/src/br/com/alura/screenmatch/principal/PrincipalComListas.java
@@ -4,9 +4,7 @@ import br.com.alura.screenmatch.modelos.Filme;
 import br.com.alura.screenmatch.modelos.Serie;
 import br.com.alura.screenmatch.modelos.Titulo;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
+import java.util.*;
 
 public class PrincipalComListas {
     static void main(String[] args) {
@@ -17,7 +15,7 @@ public class PrincipalComListas {
 
         filmeDoPaulo.avalia(10);
 
-        ArrayList<Titulo> lista = new ArrayList<>();
+        List<Titulo> lista = new LinkedList<>();
 
         lista.add(filmeDoPaulo);
         lista.add(meuFilme);
